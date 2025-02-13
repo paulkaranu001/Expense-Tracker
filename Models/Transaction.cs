9c96d61c4402d8c0bfe970bb5e3e9518203012c6
paulkaranu001 <paulkaranu@live.com>
2025-02-14 01:38:56 +0300

    Validation

--- Models/Transaction.cs
+++ Models/Transaction.cs
@@ -9,9 +9,10 @@ namespace Expense_Tracker.Models
         public int TransactionId { get; set; }
 
         // categoryId
+        [Range(1,int.MaxValue, ErrorMessage ="Please select a category")]
         public int CategoryId { get; set; }
         public Category? Category { get; set; }
-
+        [Range(1, int.MaxValue, ErrorMessage = "Amount should be more than zero")]
         public int Amount { get; set; }
 
         [Column(TypeName = "nvarchar(75)")]
