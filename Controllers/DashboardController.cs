2ba04b663d3f7dd8d245a083cbace29d0a50b8bc
paulkaranu001 <paulkaranu@live.com>
2025-02-15 01:22:03 +0300

    Dashboard Controllers

--- Controllers/DashboardController.cs
+++ Controllers/DashboardController.cs
@@ -1,11 +1,25 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Expense_Tracker.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Transactions;
 
 namespace Expense_Tracker.Controllers
 {
     public class DashboardController : Controller
     {
-        public IActionResult Index()
+        private readonly ApplicationDbContext _context;
+        public DashboardController(ApplicationDbContext context)
         {
+            _context = context;
+
+        }
+        public async IActionResult Index()
+        {
+            // Last 7 Days
+            DateTime StartDate = DateTime.Today.AddDays(-6);
+            DateTime EndDate = DateTime.Today;
+
+            List<Transaction> SelectedTransations = await _context.Transactions;
+
             return View();
         }
     }
