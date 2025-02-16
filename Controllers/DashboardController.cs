9d517abb8d8d1e5167d0ac10f9b7a53938770160
paulkaranu001 <paulkaranu@live.com>
2025-02-17 01:22:17 +0300

    DashBord Component

--- Controllers/DashboardController.cs
+++ Controllers/DashboardController.cs
@@ -1,6 +1,8 @@
 ﻿using Expense_Tracker.Models;
 using Microsoft.AspNetCore.Mvc;
-using System.Transactions;
+using Microsoft.EntityFrameworkCore;
+using System.Globalization;
+
 
 namespace Expense_Tracker.Controllers
 {
@@ -12,14 +14,34 @@ namespace Expense_Tracker.Controllers
             _context = context;
 
         }
-        public async IActionResult Index()
+        public async Task <ActionResult> Index()
         {
             // Last 7 Days
             DateTime StartDate = DateTime.Today.AddDays(-6);
             DateTime EndDate = DateTime.Today;
 
-            List<Transaction> SelectedTransations = await _context.Transactions;
+            List<Transaction> SelectedTransations = await _context.Transactions
+                .Include(x=> x.Category)
+                .Where(y => y.Date >= StartDate && y.Date < EndDate)
+                .ToListAsync();
+            //Total Income
+            int TotalIncome = SelectedTransations
+                .Where(i => i.Category.Type == "Income")
+                .Sum(J => J.Amount);
+            ViewBag.TotalIncome = TotalIncome.ToString("C0");
 
+            // Total Expense 
+            int TotalExpense = SelectedTransations
+                .Where(i => i.Category.Type == "Expense ")
+                .Sum(J => J.Amount);
+            ViewBag.TotalExpense = TotalExpense.ToString("C0");
+
+            // Balance 
+            int Balance = TotalIncome - TotalExpense;
+            ViewBag.Balance = Balance.ToString("C0");
+            CultureInfo culture = CultureInfo.CreateSpecificCulture("en-US");
+            culture.NumberFormat.CurrencyNegativePattern = 1;
+            ViewBag.Balance = String.Format(culture, "{0:C0}", Balance);
             return View();
         }
     }
